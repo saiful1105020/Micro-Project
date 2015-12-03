96a8432f48407dedc7414b23b0e75add8083bfcc
Md. Saiful Islam <saiful.11722@gmail.com>
2015-12-03 23:11:06 +0600

    some utility functions added

--- Boundary/Boundary/Boundary.c
+++ Boundary/Boundary/Boundary.c
@@ -8,6 +8,7 @@
 #define  F_CPU 1000000
 #include <avr/io.h>
 #include <util/delay.h>
+#include<stdlib.h>
 
 int8_t col_R[] = {
 	0b01111110 , 0b01101110 , 0b01011110 , 0b01001110 , 0b00111110 , 0b00101110 , 0b00011110 , 0b00001110,
@@ -21,6 +22,74 @@ int8_t boundary_port_a[] = {
 int8_t boundary_port_b[] = {
 	0xFF,0x21,0x21,0x21,0x21,0xFF,0x21,0x21,0x21,0x21,0xFF,0x21,0x21,0x21,0x21,0xFF
 };
+
+/************************************************************************/
+/*	position[i][j]:
+	-1 : Free
+	1  : Player 1
+	2  : Player 2 
+*/
+/************************************************************************/
+int8_t position[4][4];
+
+int8_t current_player;
+int8_t current_x;
+int8_t current_y;
+
+void init_control()
+{
+	int8_t i,j;
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			position[i][j]=-1;
+		}
+	}
+	
+	current_player = 1;
+	current_x = 2;
+	current_y = 2;
+}
+
+/************************************************************************/
+/* 1. Change current_player
+   2. Assign a free current_position (current_x and current_y)                                                                  */
+/************************************************************************/
+void change_control()
+{
+	if(current_player==1)
+	{
+		current_player = 2;
+	}
+	else
+	{
+		current_player = 1;
+	}
+	
+	int8_t i,j;
+	
+	int8_t isFree = 0;
+	for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=3;j++)
+		{
+			if(position[i][j]==-1)
+			{
+				current_x = i;
+				current_y = j;
+				
+				isFree = 1;
+				break;
+			}
+		}
+		
+		if(isFree==1)
+		{
+			break;
+		}
+	}
+}
 
 void draw_boundary()
 {
@@ -66,10 +135,10 @@ void draw_p1_pattern(int8_t x,int8_t y)
 			_delay_ms(0.25);
 		}
 	}
-	else if(x==1 && y==2)
+	else if(x==2 && y==1)
 	{
 		
-temp_col[0]=col_R[1];
+		temp_col[0]=col_R[1];
 		temp_col[1]=col_R[2];
 		temp_col[2]=col_R[3];
 		temp_col[3]=col_R[4];
@@ -96,7 +165,7 @@ temp_col[0]=col_R[1];
 			
 		}
 	}
-	else if(x==1 && y==3)
+	else if(x==3 && y==1)
 	{
 		temp_col[0]=col_R[1];
 		temp_col[1]=col_R[2];
@@ -120,7 +189,7 @@ temp_col[0]=col_R[1];
 	}
 	
 	
-	else if(x==2 && y==1)
+	else if(x==1 && y==2)
 	{
 		temp_col[0]=col_R[6];
 		temp_col[1]=col_R[7];
@@ -169,7 +238,7 @@ temp_col[0]=col_R[1];
 			
 		}
 	}
-	else if(x==2 && y==3)
+	else if(x==3 && y==2)
 	{
 		temp_col[0]=col_R[6];
 		temp_col[1]=col_R[7];
@@ -193,7 +262,7 @@ temp_col[0]=col_R[1];
 	}
 	
 	
-	else if(x==3 && y==1)
+	else if(x==1 && y==3)
 	{
 		temp_col[0]=col_R[11];
 		temp_col[1]=col_R[12];
@@ -213,7 +282,7 @@ temp_col[0]=col_R[1];
 			_delay_ms(0.25);
 		}
 	}
-	else if(x==3 && y==2)
+	else if(x==2 && y==3)
 	{
 		temp_col[0]=col_R[11];
 		temp_col[1]=col_R[12];
@@ -281,7 +350,7 @@ void draw_p2_pattern(int8_t x,int8_t y)
 		temp_col[3]=col_R[4];
 		
 		int8_t port_a_temp_pattern[]={
-			0x30,0x48,0x48,0x30
+			0x08,0x10,0x20,0x40
 		};
 		
 		for(i=0;i<4;i++)
@@ -293,7 +362,685 @@ void draw_p2_pattern(int8_t x,int8_t y)
 			_delay_ms(0.25);
 		}
 	}
+	else if(x==1 && y==2)
+	{
+		
+		temp_col[0]=col_R[6];
+		temp_col[1]=col_R[7];
+		temp_col[2]=col_R[8];
+		temp_col[3]=col_R[9];
+		
+		int8_t port_a_temp_pattern[]={
+			0x08,0x10,0x20,0x40
+		};
+		
+		for(i=0;i<4;i++)
+		{
+			PORTD = temp_col[i];
+			
+			PORTB = 0x00;
+			PORTA = port_a_temp_pattern[i];
+			_delay_ms(0.25);
+		}
+	}
+	else if(x==1 && y==3)
+	{
+		
+		temp_col[0]=col_R[11];
+		temp_col[1]=col_R[12];
+		temp_col[2]=col_R[13];
+		temp_col[3]=col_R[14];
+		
+		int8_t port_a_temp_pattern[]={
+			0x08,0x10,0x20,0x40
+		};
+		
+		for(i=0;i<4;i++)
+		{
+			PORTD = temp_col[i];
+			
+			PORTB = 0x00;
+			PORTA = port_a_temp_pattern[i];
+			_delay_ms(0.25);
+		}
+	}
+	else if(x==2 && y==1)
+	{
+		
+		temp_col[0]=col_R[1];
+		temp_col[1]=col_R[2];
+		temp_col[2]=col_R[3];
+		temp_col[3]=col_R[4];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x00,0x01,0x02
+		};
+		
+		int8_t port_b_temp_pattern[]={
+			0x40,0x80,0x00,0x00
+		};
+		
+		for(i=0;i<4;i++)
+		{
+			PORTD = temp_col[i];
+			
+			PORTB = 0x00;
+			PORTA = port_a_temp_pattern[i];
+			_delay_ms(0.25);
+			
+			PORTA = 0x00;
+			PORTB = port_b_temp_pattern[i];
+			_delay_ms(0.25);
+		}
+	}
+	
+	else if(x==2 && y==2)
+	{
+		
+		temp_col[0]=col_R[6];
+		temp_col[1]=col_R[7];
+		temp_col[2]=col_R[8];
+		temp_col[3]=col_R[9];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x00,0x01,0x02
+		};
+		
+		int8_t port_b_temp_pattern[]={
+			0x40,0x80,0x00,0x00
+		};
+		
+		for(i=0;i<4;i++)
+		{
+			PORTD = temp_col[i];
+			
+			PORTB = 0x00;
+			PORTA = port_a_temp_pattern[i];
+			_delay_ms(0.25);
+			
+			PORTA = 0x00;
+			PORTB = port_b_temp_pattern[i];
+			_delay_ms(0.25);
+		}
+	}
+	
+	else if(x==2 && y==3)
+	{
+		
+		temp_col[0]=col_R[11];
+		temp_col[1]=col_R[12];
+		temp_col[2]=col_R[13];
+		temp_col[3]=col_R[14];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x00,0x01,0x02
+		};
+		
+		int8_t port_b_temp_pattern[]={
+			0x40,0x80,0x00,0x00
+		};
+		
+		for(i=0;i<4;i++)
+		{
+			PORTD = temp_col[i];
+			
+			PORTB = 0x00;
+			PORTA = port_a_temp_pattern[i];
+			_delay_ms(0.25);
+			
+			PORTA = 0x00;
+			PORTB = port_b_temp_pattern[i];
+			_delay_ms(0.25);
+		}
+	}
+	
+	else if(x==3 && y==1)
+	{
+		
+		temp_col[0]=col_R[1];
+		temp_col[1]=col_R[2];
+		temp_col[2]=col_R[3];
+		temp_col[3]=col_R[4];
+		
+		int8_t port_b_temp_pattern[]={
+			0x02,0x04,0x08,0x10
+		};
+		
+		for(i=0;i<4;i++)
+		{
+			PORTD = temp_col[i];
+			
+			PORTA = 0x00;
+			PORTB = port_b_temp_pattern[i];
+			_delay_ms(0.25);
+		}
+	}
+	else if(x==3 && y==2)
+	{
+		
+		temp_col[0]=col_R[6];
+		temp_col[1]=col_R[7];
+		temp_col[2]=col_R[8];
+		temp_col[3]=col_R[9];
+		
+		int8_t port_b_temp_pattern[]={
+			0x02,0x04,0x08,0x10
+		};
+		
+		for(i=0;i<4;i++)
+		{
+			PORTD = temp_col[i];
+			
+			PORTA = 0x00;
+			PORTB = port_b_temp_pattern[i];
+			_delay_ms(0.25);
+		}
+	}
+	else if(x==3 && y==3)
+	{
+		
+		temp_col[0]=col_R[11];
+		temp_col[1]=col_R[12];
+		temp_col[2]=col_R[13];
+		temp_col[3]=col_R[14];
+		
+		int8_t port_b_temp_pattern[]={
+			0x02,0x04,0x08,0x10
+		};
+		
+		for(i=0;i<4;i++)
+		{
+			PORTD = temp_col[i];
+			
+			PORTA = 0x00;
+			PORTB = port_b_temp_pattern[i];
+			_delay_ms(0.25);
+		}
+	}
 }
+
+void draw_occupied_positions()
+{
+	int8_t i,j;
+	
+	/************************************************************************/
+	/* TEST BLOCK                                                           */
+	/************************************************************************/
+	
+	position[1][2]=1;
+	position[2][3]=1;
+	position[3][2]=2;
+	position[3][1]=2;
+	
+	/************************************************************************/
+	/* END OF TEST BLOCK                                                                     */
+	/************************************************************************/
+	
+	for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=3;j++)
+		{
+			if(position[i][j]==-1)
+			{
+				//Do Nothing
+			}
+			else if(position[i][j]==1)
+			{
+				draw_p1_pattern(i,j);
+			}
+			else
+			{
+				draw_p2_pattern(i,j);
+			}
+		}
+	}
+}
+
+void draw_blinking_pattern()
+{
+	int8_t temp_col[4];
+	
+	int8_t i;
+	
+	if(current_player==1 && current_x==1 && current_y==1)
+	{
+		
+		temp_col[0]=col_R[1];
+		temp_col[1]=col_R[2];
+		temp_col[2]=col_R[3];
+		temp_col[3]=col_R[4];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x30,0x30,0x00
+		};
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = port_a_temp_pattern[i];
+					_delay_ms(0.20);
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+	
+	else if(current_player==1 && current_x==1 && current_y==2)
+	{
+		
+		temp_col[0]=col_R[6];
+		temp_col[1]=col_R[7];
+		temp_col[2]=col_R[8];
+		temp_col[3]=col_R[9];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x30,0x30,0x00
+		};
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = port_a_temp_pattern[i];
+					_delay_ms(0.20);
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+	
+	else if(current_player==1 && current_x==1 && current_y==3)
+	{
+		
+		temp_col[0]=col_R[11];
+		temp_col[1]=col_R[12];
+		temp_col[2]=col_R[13];
+		temp_col[3]=col_R[14];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x30,0x30,0x00
+		};
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = port_a_temp_pattern[i];
+					_delay_ms(0.20);
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+	
+	else if(current_player==1 && current_x==2 && current_y==1)
+	{
+		
+		temp_col[0]=col_R[1];
+		temp_col[1]=col_R[2];
+		temp_col[2]=col_R[3];
+		temp_col[3]=col_R[4];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x01,0x01,0x00
+		};
+		
+		int8_t port_b_temp_pattern[]={
+			0x00,0x80,0x80,0x00
+		};
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = port_a_temp_pattern[i];
+					_delay_ms(0.25);
+					
+					PORTA = 0x00;
+					PORTB = port_b_temp_pattern[i];
+					_delay_ms(0.25);
+					
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+	
+	else if(current_player==1 && current_x==2 && current_y==2)
+	{
+		
+		temp_col[0]=col_R[6];
+		temp_col[1]=col_R[7];
+		temp_col[2]=col_R[8];
+		temp_col[3]=col_R[9];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x01,0x01,0x00
+		};
+		
+		int8_t port_b_temp_pattern[]={
+			0x00,0x80,0x80,0x00
+		};
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = port_a_temp_pattern[i];
+					_delay_ms(0.25);
+					
+					PORTA = 0x00;
+					PORTB = port_b_temp_pattern[i];
+					_delay_ms(0.25);
+					
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+	
+	else if(current_player==1 && current_x==2 && current_y==3)
+	{
+		
+		temp_col[0]=col_R[11];
+		temp_col[1]=col_R[12];
+		temp_col[2]=col_R[13];
+		temp_col[3]=col_R[14];
+		
+		int8_t port_a_temp_pattern[]={
+			0x00,0x01,0x01,0x00
+		};
+		
+		int8_t port_b_temp_pattern[]={
+			0x00,0x80,0x80,0x00
+		};
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = port_a_temp_pattern[i];
+					_delay_ms(0.25);
+					
+					PORTA = 0x00;
+					PORTB = port_b_temp_pattern[i];
+					_delay_ms(0.25);
+					
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+	
+	else if(current_player==1 && current_x==3 && current_y==1)
+	{
+		temp_col[0]=col_R[1];
+		temp_col[1]=col_R[2];
+		temp_col[2]=col_R[3];
+		temp_col[3]=col_R[4];
+		
+		int8_t port_b_temp_pattern[]={
+			0x00,0x0C,0x0C,0x00
+		};
+		
+		
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTA = 0x00;
+					PORTB = port_b_temp_pattern[i];
+					_delay_ms(0.25);
+					
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+	
+	else if(current_player==1 && current_x==3 && current_y==2)
+	{
+		temp_col[0]=col_R[6];
+		temp_col[1]=col_R[7];
+		temp_col[2]=col_R[8];
+		temp_col[3]=col_R[9];
+		
+		int8_t port_b_temp_pattern[]={
+			0x00,0x0C,0x0C,0x00
+		};
+		
+		
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTA = 0x00;
+					PORTB = port_b_temp_pattern[i];
+					_delay_ms(0.25);
+					
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+	
+	else if(current_player==1 && current_x==3 && current_y==3)
+	{
+		temp_col[0]=col_R[11];
+		temp_col[1]=col_R[12];
+		temp_col[2]=col_R[13];
+		temp_col[3]=col_R[14];
+		
+		int8_t port_b_temp_pattern[]={
+			0x00,0x0C,0x0C,0x00
+		};
+		
+		
+		
+		int8_t loop;
+		for(loop=0;loop<10;loop++)
+		{
+			if(loop%2==0)
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTA = 0x00;
+					PORTB = port_b_temp_pattern[i];
+					_delay_ms(0.25);
+					
+				}
+			}
+			else
+			{
+				for(i=0;i<4;i++)
+				{
+					PORTD = temp_col[i];
+					
+					PORTB = 0x00;
+					PORTA = 0x00;
+					
+					_delay_ms(0.20);
+				}
+				
+				draw_boundary();
+				draw_occupied_positions();
+				_delay_ms(1);
+			}
+		}
+	}
+}
+
 
 int main(void)
 {
@@ -306,31 +1053,22 @@ int main(void)
 	DDRC = 0x00;
 	DDRD = 0xFF;
 	
+	init_control();
+	
     while(1)
     {
         //TODO:: Please write your application code
 		draw_boundary();
-		/*
-		draw_p1_pattern(1,1);
-		draw_p1_pattern(1,2);
-		draw_p1_pattern(1,3);
-		draw_p1_pattern(2,1);
-		draw_p1_pattern(2,2);
-		draw_p1_pattern(2,3);
-		*/
-		int8_t test_loop_i;
-		
-		for(test_loop_i=1;test_loop_i<=3;test_loop_i++)
-		{
-			int8_t test_loop_j;
-			for(test_loop_j=1;test_loop_j<=3;test_loop_j++)
-			{
-				if(test_loop_i==1 && test_loop_j==1)
-					draw_p2_pattern(1,1);
-				else
-					draw_p1_pattern(test_loop_i,test_loop_j);
-			}
-		}
-			
+		draw_occupied_positions();
+		/************************************************************************/
+		/* TEST BLOCK                                                           */
+		/************************************************************************/
+		current_player=1;
+		current_x=3;
+		current_y=3;
+		/************************************************************************/
+		/* END OF TEST BLOCK                                                    */
+		/************************************************************************/
+		draw_blinking_pattern();	
     }
 }
--- Boundary/Boundary/Debug/Boundary.d
+++ Boundary/Boundary/Debug/Boundary.d
@@ -12,7 +12,9 @@ Boundary.d Boundary.o: .././Boundary.c \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -41,3 +43,7 @@ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-g
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h:
